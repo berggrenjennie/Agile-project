a6f251091e862d64be76d962a2fd5135eb23bc80
FatmeMustafa <fatme.mustafa888@gmail.com>
2020-01-08 22:46:29 +0100

    commercial Component done - BUT missing payment options image

---
+++ frontend/src/components/CommercialComponent.js
@@ -0,0 +1,36 @@
+//core functionality from React.
+import React, { Component } from 'react';
+
+//CSS imports.
+import '../styles/Commercial.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+//image imports.
+import commercialImg from '../images/commercial1.jpg';
+import logoNoTextImg from '../images/logoNoText.jpg';
+import paymentImg from '../images/Payment.PNG';
+
+class CommercialComponent extends Component {
+  render() {
+    return (
+      <div className='commercialContainer'>
+        <div className='newsletterDiv'>
+            <img className='logoImg' src={logoNoTextImg} alt='BellusLogo'/>
+            <span className='Bellus'>Bellus</span>
+            <p className='nyhetsbrevP'>Nyhetsbrev</p>
+            <form>
+                <i className="far fa-envelope"></i>
+                <input type='email' placeholder='Email'/>
+            </form>
+            <p>Anmäl</p>
+        </div>
+        <div className='commercialDiv'>
+            <img className='commercialImg' src={commercialImg} alt='commercial'/>
+            <p>Behöver din baby nya kläder? - köp dem hos oss!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CommercialComponent;
